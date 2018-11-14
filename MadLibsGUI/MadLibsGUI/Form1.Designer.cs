0154701a285ef1434606bdab76a7fd2c1ee0f6c6
Nick-Nahnybida <nicknahnybida@gmail.com>
2018-11-14 13:03:09 -0600

    update

--- MadLibsGUI/MadLibsGUI/Form1.Designer.cs
+++ MadLibsGUI/MadLibsGUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MadLibsGUI
 {
-    partial class Form1
+    partial class frmMadLibsGUI
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.lblWordEndingEST = new System.Windows.Forms.Label();
+            this.lblBodyPartPlural = new System.Windows.Forms.Label();
+            this.lblAnimal = new System.Windows.Forms.Label();
+            this.lblNoun = new System.Windows.Forms.Label();
+            this.lblPluralNoun = new System.Windows.Forms.Label();
+            this.lblNumberA = new System.Windows.Forms.Label();
+            this.lblNumberB = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtWordEST = new System.Windows.Forms.TextBox();
+            this.txtBodyPartPlural = new System.Windows.Forms.TextBox();
+            this.txtAnimal = new System.Windows.Forms.TextBox();
+            this.txtNoun = new System.Windows.Forms.TextBox();
+            this.txtPluralNoun = new System.Windows.Forms.TextBox();
+            this.txtNumberA = new System.Windows.Forms.TextBox();
+            this.txtNumberB = new System.Windows.Forms.TextBox();
+            this.btnCreateStory = new System.Windows.Forms.Button();
+            this.lblStoryResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(70, 50);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(44, 13);
+            this.lblColor.TabIndex = 0;
+            this.lblColor.Text = "A Color:";
+            // 
+            // lblWordEndingEST
+            // 
+            this.lblWordEndingEST.AutoSize = true;
+            this.lblWordEndingEST.Location = new System.Drawing.Point(11, 78);
+            this.lblWordEndingEST.Name = "lblWordEndingEST";
+            this.lblWordEndingEST.Size = new System.Drawing.Size(103, 13);
+            this.lblWordEndingEST.TabIndex = 1;
+            this.lblWordEndingEST.Text = "Word Ending in -est:";
+            // 
+            // lblBodyPartPlural
+            // 
+            this.lblBodyPartPlural.AutoSize = true;
+            this.lblBodyPartPlural.Location = new System.Drawing.Point(29, 113);
+            this.lblBodyPartPlural.Name = "lblBodyPartPlural";
+            this.lblBodyPartPlural.Size = new System.Drawing.Size(85, 13);
+            this.lblBodyPartPlural.TabIndex = 2;
+            this.lblBodyPartPlural.Text = "Body Part Plural:";
+            // 
+            // lblAnimal
+            // 
+            this.lblAnimal.AutoSize = true;
+            this.lblAnimal.Location = new System.Drawing.Point(57, 140);
+            this.lblAnimal.Name = "lblAnimal";
+            this.lblAnimal.Size = new System.Drawing.Size(57, 13);
+            this.lblAnimal.TabIndex = 3;
+            this.lblAnimal.Text = "An Animal:";
+            // 
+            // lblNoun
+            // 
+            this.lblNoun.AutoSize = true;
+            this.lblNoun.Location = new System.Drawing.Point(68, 171);
+            this.lblNoun.Name = "lblNoun";
+            this.lblNoun.Size = new System.Drawing.Size(46, 13);
+            this.lblNoun.TabIndex = 4;
+            this.lblNoun.Text = "A Noun:";
+            // 
+            // lblPluralNoun
+            // 
+            this.lblPluralNoun.AutoSize = true;
+            this.lblPluralNoun.Location = new System.Drawing.Point(39, 204);
+            this.lblPluralNoun.Name = "lblPluralNoun";
+            this.lblPluralNoun.Size = new System.Drawing.Size(75, 13);
+            this.lblPluralNoun.TabIndex = 5;
+            this.lblPluralNoun.Text = "A Plural Noun:";
+            // 
+            // lblNumberA
+            // 
+            this.lblNumberA.AutoSize = true;
+            this.lblNumberA.Location = new System.Drawing.Point(57, 238);
+            this.lblNumberA.Name = "lblNumberA";
+            this.lblNumberA.Size = new System.Drawing.Size(57, 13);
+            this.lblNumberA.TabIndex = 6;
+            this.lblNumberA.Text = "A Number:";
+            // 
+            // lblNumberB
+            // 
+            this.lblNumberB.AutoSize = true;
+            this.lblNumberB.Location = new System.Drawing.Point(57, 268);
+            this.lblNumberB.Name = "lblNumberB";
+            this.lblNumberB.Size = new System.Drawing.Size(57, 13);
+            this.lblNumberB.TabIndex = 7;
+            this.lblNumberB.Text = "A Number:";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(140, 50);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 20);
+            this.txtColor.TabIndex = 8;
+            // 
+            // txtWordEST
+            // 
+            this.txtWordEST.Location = new System.Drawing.Point(140, 78);
+            this.txtWordEST.Name = "txtWordEST";
+            this.txtWordEST.Size = new System.Drawing.Size(100, 20);
+            this.txtWordEST.TabIndex = 9;
+            // 
+            // txtBodyPartPlural
+            // 
+            this.txtBodyPartPlural.Location = new System.Drawing.Point(140, 113);
+            this.txtBodyPartPlural.Name = "txtBodyPartPlural";
+            this.txtBodyPartPlural.Size = new System.Drawing.Size(100, 20);
+            this.txtBodyPartPlural.TabIndex = 10;
+            // 
+            // txtAnimal
+            // 
+            this.txtAnimal.Location = new System.Drawing.Point(140, 140);
+            this.txtAnimal.Name = "txtAnimal";
+            this.txtAnimal.Size = new System.Drawing.Size(100, 20);
+            this.txtAnimal.TabIndex = 11;
+            // 
+            // txtNoun
+            // 
+            this.txtNoun.Location = new System.Drawing.Point(140, 171);
+            this.txtNoun.Name = "txtNoun";
+            this.txtNoun.Size = new System.Drawing.Size(100, 20);
+            this.txtNoun.TabIndex = 12;
+            // 
+            // txtPluralNoun
+            // 
+            this.txtPluralNoun.Location = new System.Drawing.Point(140, 204);
+            this.txtPluralNoun.Name = "txtPluralNoun";
+            this.txtPluralNoun.Size = new System.Drawing.Size(100, 20);
+            this.txtPluralNoun.TabIndex = 13;
+            // 
+            // txtNumberA
+            // 
+            this.txtNumberA.Location = new System.Drawing.Point(140, 238);
+            this.txtNumberA.Name = "txtNumberA";
+            this.txtNumberA.Size = new System.Drawing.Size(100, 20);
+            this.txtNumberA.TabIndex = 14;
+            // 
+            // txtNumberB
+            // 
+            this.txtNumberB.Location = new System.Drawing.Point(140, 268);
+            this.txtNumberB.Name = "txtNumberB";
+            this.txtNumberB.Size = new System.Drawing.Size(100, 20);
+            this.txtNumberB.TabIndex = 15;
+            // 
+            // btnCreateStory
+            // 
+            this.btnCreateStory.Location = new System.Drawing.Point(305, 73);
+            this.btnCreateStory.Name = "btnCreateStory";
+            this.btnCreateStory.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateStory.TabIndex = 16;
+            this.btnCreateStory.Text = "Create Story";
+            this.btnCreateStory.UseVisualStyleBackColor = true;
+            this.btnCreateStory.Click += new System.EventHandler(this.btnCreateStory_Click);
+            // 
+            // lblStoryResult
+            // 
+            this.lblStoryResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStoryResult.Location = new System.Drawing.Point(305, 113);
+            this.lblStoryResult.Name = "lblStoryResult";
+            this.lblStoryResult.Size = new System.Drawing.Size(483, 328);
+            this.lblStoryResult.TabIndex = 17;
+            // 
+            // frmMadLibsGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblStoryResult);
+            this.Controls.Add(this.btnCreateStory);
+            this.Controls.Add(this.txtNumberB);
+            this.Controls.Add(this.txtNumberA);
+            this.Controls.Add(this.txtPluralNoun);
+            this.Controls.Add(this.txtNoun);
+            this.Controls.Add(this.txtAnimal);
+            this.Controls.Add(this.txtBodyPartPlural);
+            this.Controls.Add(this.txtWordEST);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.lblNumberB);
+            this.Controls.Add(this.lblNumberA);
+            this.Controls.Add(this.lblPluralNoun);
+            this.Controls.Add(this.lblNoun);
+            this.Controls.Add(this.lblAnimal);
+            this.Controls.Add(this.lblBodyPartPlural);
+            this.Controls.Add(this.lblWordEndingEST);
+            this.Controls.Add(this.lblColor);
+            this.Name = "frmMadLibsGUI";
+            this.Text = "MadLibGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Label lblWordEndingEST;
+        private System.Windows.Forms.Label lblBodyPartPlural;
+        private System.Windows.Forms.Label lblAnimal;
+        private System.Windows.Forms.Label lblNoun;
+        private System.Windows.Forms.Label lblPluralNoun;
+        private System.Windows.Forms.Label lblNumberA;
+        private System.Windows.Forms.Label lblNumberB;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtWordEST;
+        private System.Windows.Forms.TextBox txtBodyPartPlural;
+        private System.Windows.Forms.TextBox txtAnimal;
+        private System.Windows.Forms.TextBox txtNoun;
+        private System.Windows.Forms.TextBox txtPluralNoun;
+        private System.Windows.Forms.TextBox txtNumberA;
+        private System.Windows.Forms.TextBox txtNumberB;
+        private System.Windows.Forms.Button btnCreateStory;
+        private System.Windows.Forms.Label lblStoryResult;
     }
 }
 
--- MadLibsGUI/MadLibsGUI/Form1.cs
+++ MadLibsGUI/MadLibsGUI/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace MadLibsGUI
 {
-    public partial class Form1 : Form
+    public partial class frmMadLibsGUI : Form
     {
-        public Form1()
+        public frmMadLibsGUI()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateStory_Click(object sender, EventArgs e)
+        {
+            string color;
+            string wordest;
+            string bodyPartPlural;
+            string Animal;
+            string Noun;
+            string pluralNoun;
+            int numberA;
+            int numberB;
+            int NumberC;
+
+            color = txtColor.Text;
+            wordest = txtWordEST.Text;
+            bodyPartPlural = txtBodyPartPlural.Text;
+            Animal = txtAnimal.Text;
+            Noun = txtNoun.Text;
+            pluralNoun = txtPluralNoun.Text;
+            numberA = Convert.ToInt32(txtNumberA.Text);
+            numberB = Convert.ToInt32(txtNumberB.Text);
+            NumberC = (numberA - numberB);
+
+            lblStoryResult.Text = "The " + color + " Dragon is the " + wordest + " Dragon of all. It has " + NumberC + " " + bodyPartPlural + ", and a " + Animal + " shaped like a " + Noun + ". It loves to eat " + pluralNoun + ", although it will feast on nearly anything.";
         }
     }
 }
